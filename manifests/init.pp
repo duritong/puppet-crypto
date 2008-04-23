32db9dfe0b96ccc147b5d9408d5a88be09a02b60
mh <mh@immerda.ch>
2008-04-23 18:32:15 +0000

    refactored and fixed it

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,23 +1,35 @@
 # modules/crypto/manifests/init.pp - manage crypto stuff
 # Copyright (C) 2007 admin@immerda.ch
+# GPLv3
+# This modules mainly setup sime basic crypto stuff
 #
 
 #modules_dir { "crypto": }
 
 class crypto {
-        case $kernel {
-                linux: { include crypto::luks }
-                openbsd: { include crypto::openbsd }
-        }
+    case $operatingsystem {
+        centos: { include crypto::luks::centos }
+        gentoo: { include crypto::luks::gentoo }
+        openbsd: { include crypto::openbsd }
+        default: { include crypto::luks::base }
+    }
 }
 
-class crypto::luks {
+class crypto::linux::base {
     package{cryptsetup:
         ensure => present,
-        category => $operatingsystem ? {
-            gentoo => 'sys-fs',
-            default => '',
-        },
+    }
+}
+
+class crypto::linux::centos inherits crypto::linux::base {    
+    Package[cryptsetup] {
+        name => 'cryptsetup-luks',
+    }
+}
+
+class crypto::linux::gentoo inherits crypto::linux::base {    
+    Package[cryptsetup] {
+        category => 'sys-fs',
     }
 }
 
